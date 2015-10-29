75d96675d0ea5f04f4e427e4c46c93497b6b3190
Robert Säll <pr_125@hotmail.com>
2015-10-29 10:27:43 +0100

    Also uploads a statusfile and changes of arguments

--- src/pusher/uploader.go
+++ src/pusher/uploader.go
@@ -8,6 +8,8 @@ import (
     "encoding/hex"
     "os"
     "path/filepath"
+    "bytes"
+    "encoding/json"
 )
 
 func main() {
@@ -20,17 +22,22 @@ func main() {
         Tenant:   "",  // Name of the tenant (v2 auth only)
     }
 
-    if (len(os.Args) < 3) {
-        fmt.Printf("Usage: %s path container_name\n", os.Args[0])
+    if (len(os.Args) < 5) {
+        fmt.Printf("Usage: %s customer jobId status path\n", os.Args[0])
         os.Exit(1)
     }
 
     println("Starting objekt storage uploader")
 
     var (
-        path = os.Args[1]
-        container_name = os.Args[2]
+        customer = os.Args[1]
+        jobId = os.Args[2]
+        status = os.Args[3]
+        path = os.Args[4]
         )
+
+    container_name := "jobs-" + customer
+    file_prefix := jobId
 
     // Authenticate
     err := c.Authenticate()
@@ -38,11 +45,72 @@ func main() {
         panic(err)
     }
 
-    uploadContentsInFolder(path, container_name, c)
+    uploadContentsInFolder(path, file_prefix, container_name, c)
 
-    // List all the containers
-    containers, err := c.ContainerNames(nil)
-    fmt.Println(containers)
+    updateStatusfile(container_name, jobId, status, c)
+}
+
+type Jobinfo struct {
+    JobId string `json:"jobId"`
+    Status string `json:"status"`
+}
+
+
+type Jobinfos struct {
+    Infos [] Jobinfo `json:"jobs"`
+}
+
+
+func updateStatusfile(container string, jobId string, status string, c swift.Connection) {
+    buf := new(bytes.Buffer)
+    _, err := c.ObjectGet(container, "jobs.json", buf, true, nil)
+
+    info := Jobinfo{jobId, status}
+    s, _ := json.Marshal(info)
+    println(string(s))
+    var content []byte = nil
+
+    if (err != nil) {
+        println("jobs.json doesn't exists, will create new file")
+        // Does not exists, corruped or otherwise, recreate it
+        infos := Jobinfos{}
+        infos.Infos = append(infos.Infos, info)
+        b, err := json.Marshal(infos)
+        if (err != nil) {
+            println("Couldn't marshal json data, won't create jobs.json")
+            return
+        } else {
+            content = b
+        }
+    } else {
+        println("File exists, will append to it")
+        infos := Jobinfos{}
+        err := json.Unmarshal(buf.Bytes(), &infos)
+        if (err != nil) {
+            println("Couldn't unmarshal current json data, won't update jobs.json")
+            return
+        }
+        infos.Infos = append(infos.Infos, info)
+        b, err := json.Marshal(infos)
+        if (err != nil) {
+            println("Couldn't marshal json data, won't create jobs.json")
+            return
+        } else {
+            content = b
+        }
+    }
+
+    hasher := md5.New()
+    hasher.Write(content)
+    md5hash := hex.EncodeToString(hasher.Sum(nil))
+
+    file, err := c.ObjectCreate(container, "jobs.json", false, md5hash, "json", nil)
+    if (err != nil) {
+        println(err.Error())
+    } else {
+        file.Write(content)
+    }
+    file.Close()
 }
 
 func getContentFromContainer(container string, c swift.Connection) {
@@ -55,11 +123,11 @@ func getContentFromContainer(container string, c swift.Connection) {
 }
 
 
-func uploadContentsInFolder(path string, container string, c swift.Connection) {
+func uploadContentsInFolder(path string, prefix string, container string, c swift.Connection) {
     createContainer(container, c)
 
     err := filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
-        uploadFile(container, path, c)
+        uploadFile(container, prefix, path, c)
         return nil
     })
     if (err != nil) {
@@ -71,12 +139,12 @@ func createContainer(name string, c swift.Connection) {
     c.ContainerCreate(name, nil)
 }
 
-func uploadFile(container string, path string, c swift.Connection) {
+func uploadFile(container string, prefix string, path string, c swift.Connection) {
     dat, err := ioutil.ReadFile(path)
     if (err != nil) {
         println(err.Error())
     } else {
-        name := filepath.Base(path)
+        name := prefix + "-" + filepath.Base(path)
         ext := filepath.Ext(path)
         hasher := md5.New()
         hasher.Write(dat)
