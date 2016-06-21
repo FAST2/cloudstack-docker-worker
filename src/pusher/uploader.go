11dc1adbade0c566d94536562f1e46913e949562
Robert Säll <robert.sall@fast2.se>
2016-06-21 13:52:20 +0200

    Using functions from shared library

--- src/pusher/uploader.go
+++ src/pusher/uploader.go
@@ -3,15 +3,10 @@ package main
 import (
     "github.com/ncw/swift"
     "fmt"
-    "crypto/md5"
-    "io/ioutil"
-    "encoding/hex"
     "os"
     "path/filepath"
-    "bytes"
-    "encoding/json"
     "fast2.se/swifthelper"
-    //"strings"
+    "github.com/fast2/wpaumetadata"
 )
 
 func main() {
@@ -48,82 +43,8 @@ func main() {
     }
 
     uploadContentsInFolder(path, file_prefix, container_name, c)
-
-    updateStatusfile(container_name, jobId, status, c)
+    wpaumetadata.Add(c, container_name, jobId, status)
 }
-
-type Jobinfo struct {
-    JobId string `json:"jobId"`
-    Status string `json:"status"`
-}
-
-
-type Jobinfos struct {
-    Infos [] Jobinfo `json:"jobs"`
-}
-
-
-func updateStatusfile(container string, jobId string, status string, c swift.Connection) {
-    buf := new(bytes.Buffer)
-    _, err := c.ObjectGet(container, "jobs.json", buf, true, nil)
-
-    info := Jobinfo{jobId, status}
-    s, _ := json.Marshal(info)
-    println(string(s))
-    var content []byte = nil
-
-    if (err != nil) {
-        println("jobs.json doesn't exists, will create new file")
-        // Does not exists, corruped or otherwise, recreate it
-        infos := Jobinfos{}
-        infos.Infos = append(infos.Infos, info)
-        b, err := json.Marshal(infos)
-        if (err != nil) {
-            println("Couldn't marshal json data, won't create jobs.json")
-            return
-        } else {
-            content = b
-        }
-    } else {
-        println("File exists, will append to it")
-        infos := Jobinfos{}
-        err := json.Unmarshal(buf.Bytes(), &infos)
-        if (err != nil) {
-            println("Couldn't unmarshal current json data, won't update jobs.json")
-            return
-        }
-        infos.Infos = append(infos.Infos, info)
-        b, err := json.Marshal(infos)
-        if (err != nil) {
-            println("Couldn't marshal json data, won't create jobs.json")
-            return
-        } else {
-            content = b
-        }
-    }
-
-    hasher := md5.New()
-    hasher.Write(content)
-    md5hash := hex.EncodeToString(hasher.Sum(nil))
-
-    file, err := c.ObjectCreate(container, "jobs.json", false, md5hash, "json", nil)
-    if (err != nil) {
-        println(err.Error())
-    } else {
-        file.Write(content)
-    }
-    file.Close()
-}
-
-func getContentFromContainer(container string, c swift.Connection) {
-    names, _ := c.ObjectNamesAll(container, nil)
-
-    for i := range names {
-        println(names[i])
-        c.ObjectGet(container, names[i], os.Stdout, true, nil)
-    }
-}
-
 
 func uploadContentsInFolder(path string, prefix string, container string, c swift.Connection) {
     swifthelper.CreatePublicContainer(container, c)
