676f2956ec5b4f72fd441bd5f61c974a6f3906bc
Daniel de Oquiñena <daniel@fast2.se>
2022-04-05 15:31:13 +0200

    Update to swift/v2

--- src/cleaner/cleaner.go
+++ src/cleaner/cleaner.go
@@ -1,57 +1,58 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 
-	"github.com/fast2/wpaumetadata"
-	"github.com/ncw/swift"
+	"github.com/FAST2/wpaumetadata"
+	"github.com/ncw/swift/v2"
 )
 
 const MAX_JOBS = 60 // Two months
 
 func main() {
-	// Create a connection
-	c := swift.Connection{
-		UserName: os.Getenv("SWIFT_API_USER"),
-		ApiKey:   os.Getenv("SWIFT_API_KEY"),
-		AuthUrl:  os.Getenv("SWIFT_AUTH_URL"),
-		Domain:   os.Getenv("SWIFT_API_DOMAIN"), // Name of the domain (v3 auth only)
-		Tenant:   os.Getenv("SWIFT_TENANT"), // Name of the tenant (v2 auth only)
+	ctx := context.Background()
+	// Create a connection using openstack v3applicationcredential
+	c := &swift.Connection{
+		ApplicationCredentialId:     os.Getenv("OS_APPLICATION_CREDENTIAL_ID"),
+		ApplicationCredentialSecret: os.Getenv("OS_APPLICATION_CREDENTIAL_SECRET"),
+		AuthUrl:                     os.Getenv("OS_AUTH_URL"),
 	}
 
 	println("Starting cleaner")
 
 	// Authenticate
-	err := c.Authenticate()
+	err := c.Authenticate(ctx)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error authenticating: %s\n", err)
+		os.Exit(1)
 	}
 
-	containers := get_projects(c)
+	containers := get_projects(ctx, *c)
 
 	fmt.Println("Found job containers:")
 	fmt.Println(containers)
 
 	for _, val := range containers {
-		clean_old_jobs(c, val)
+		clean_old_jobs(ctx, *c, val)
 	}
 }
 
-func get_projects(c swift.Connection) []string {
+func get_projects(ctx context.Context, c swift.Connection) []string {
 	opts := new(swift.ContainersOpts)
 	opts.Prefix = "jobs"
 
-	containers, err := c.ContainerNames(opts)
+	containers, err := c.ContainerNames(ctx, opts)
 	if err != nil {
 		panic(err)
 	}
 	return containers
 }
 
-func clean_old_jobs(c swift.Connection, container string) {
-	buf, err := wpaumetadata.GetMetadata(c, container)
+func clean_old_jobs(ctx context.Context, c swift.Connection, container string) {
+	buf, err := wpaumetadata.GetMetadata(ctx, c, container)
 	if err != nil {
 		fmt.Printf("No metadata file for %s\n", container)
 		return
@@ -71,9 +72,9 @@ func clean_old_jobs(c swift.Connection, container string) {
 		for i := 0; i < remove_count; i++ {
 			prefix := infos.Infos[i].JobId
 			fmt.Printf("Will iterate over contents of %s with prefix %s for removal of items\n", container, prefix)
-			objects_to_remove := get_objects(c, container, prefix)
+			objects_to_remove := get_objects(ctx, c, container, prefix)
 			fmt.Printf("Items to be removed: %s\n", objects_to_remove)
-			remove_objects(c, container, objects_to_remove)
+			remove_objects(ctx, c, container, objects_to_remove)
 			infos = remove_from_metadata(infos, prefix)
 		}
 	}
@@ -82,26 +83,26 @@ func clean_old_jobs(c swift.Connection, container string) {
 	if err != nil {
 		fmt.Printf("Couldn't marshal to JSON with updated for %s", container)
 	} else {
-		wpaumetadata.Upload(c, container, updated_metadata)
+		wpaumetadata.Upload(ctx, c, container, updated_metadata)
 	}
 
 }
 
-func get_objects(c swift.Connection, container string, prefix string) []string {
+func get_objects(ctx context.Context, c swift.Connection, container string, prefix string) []string {
 	opts := new(swift.ObjectsOpts)
 	opts.Prefix = prefix
-	names, err := c.ObjectNames(container, opts)
+	names, err := c.ObjectNames(ctx, container, opts)
 	if err != nil {
 		panic(err)
 	}
 	return names
 }
 
-func remove_objects(c swift.Connection, container string, names []string) bool {
+func remove_objects(ctx context.Context, c swift.Connection, container string, names []string) bool {
 	removed_all := true
 	for _, val := range names {
 		fmt.Printf("Removing %s\n", val)
-		err := c.ObjectDelete(container, val)
+		err := c.ObjectDelete(ctx, container, val)
 		if err != nil {
 			fmt.Printf("Couldn't delete '%s' because: %s\n", val, err.Error())
 			removed_all = false
---
+++ src/cleaner/go.mod
@@ -0,0 +1,8 @@
+module cleaner
+
+go 1.18
+
+require (
+	github.com/FAST2/wpaumetadata v0.0.0-20220405125455-97d875563dc0
+	github.com/ncw/swift/v2 v2.0.1
+)
